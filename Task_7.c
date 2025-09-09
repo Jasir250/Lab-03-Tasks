6a833c472b1e764d367b58bee68fd9f48062ed28
Jasir <k250701@nu.edu.pk>
2025-09-10 00:11:50 +0500

    Create Task_7.c

---
+++ Task_7.c
@@ -0,0 +1,19 @@
+// Task #7
+#include <stdio.h>
+
+int main() {
+    int length;
+    int width;
+    int area;
+    int perimeter;
+    printf("Welcome to the rectangle area and perimeter calculator\n");
+    printf("Enter width\n");
+    scanf("%d", &width);
+    printf("Enter length\n");
+    scanf("%d", &length);
+    area = length * width ;
+    perimeter = 2 * (length + width);
+    printf("The area is: %d\n", area);
+    printf("The perimeter is: %d", perimeter);
+    
+}
